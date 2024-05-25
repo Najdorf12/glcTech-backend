41e8401d9b23b3d885d25ba419ebad6e9b1aa149
Agustin Morro <agustin.morro@gmail.com>
2024-05-25 18:41:47 -0300

    add controllers

--- src/controllers/products.controller.js
+++ src/controllers/products.controller.js
@@ -5,51 +5,34 @@ export const getProducts = async (req, res) => {
   res.json(products);
 };
 
-/* export const createProduct = async (req, res) => {
-  const { name, description, category, price, date } = req.body;
+ export const createProduct = async (req, res) => {
+  const { name, description, category, price } = req.body;
   try {
     const newProduct = new Product({
       name,
       description,
       price,
-      category,
-      date,
+      category
     });
-
-    if (req?.files.image) {
-      const result = await uploadImage(req.files.image.tempFilePath);
-      newProduct.image = {
-        public_id: result.public_id,
-        secure_url: result.secure_url,
-      };
-      await fs.unlink(req.files.image.tempFilePath);
-    }
-
+    
     const savedProduct = await newProduct.save();
     res.json(savedProduct);
+
   } catch (error) {
     console.error(error);
   }
 };
 
-export const getProduct = async (req, res) => {
-  const product = await Product.findById(req.params.id);
-  if (!product) return res.status(404).json({ message: "Product not found" });
-  res.json(product);
-};
-
 export const deleteProduct = async (req, res) => {
   try {
-    const product = await Product.findByIdAndDelete(req.params.id);l
+    const product = await Product.findByIdAndDelete(req.params.id);
     if (!product) return res.status(404).json({ message: "Product not found" });
-
-    await deleteImage(product.image.public_id);
     res.json(product);
+
   } catch (error) {
     return res.status(500).json({ message: error.message });
   }
 };
-
 export const updateProduct = async (req, res) => {
   const product = await Product.findByIdAndUpdate(req.params.id, req.body, {
     new: true,
@@ -68,13 +51,3 @@ export const getProductByCategory = async (req, res) => {
     return res.status(404).json({ message: "Product not found" });
   res.json(productsFilter);
 };
-
-export const getProductByName = async (req, res) => {
-  const name = req.params.name;
-  const products = await Product.find();
-  const productsFilter = products.filter((product) => product.name === name);
-  if (!productsFilter)
-    return res.status(404).json({ message: "Products not found" });
-
-  res.json(productsFilter);
-}; */
--- src/routes/products.routes.js
+++ src/routes/products.routes.js
@@ -1,25 +1,19 @@
 import { Router } from "express";
 import {
   getProducts,
-/*   getProduct,
   createProduct,
   deleteProduct,
   updateProduct,
   getProductByCategory,
-  getProductByName */
-} from "../controllers/products.controller.js"; 
+} from "../controllers/products.controller.js";
 
 const router = Router();
 
 router.get("/", getProducts);
-/* router.get("/:id", getProduct);
+router.post("/", createProduct);
+router.put("/:id", updateProduct);
+router.delete("/:id", deleteProduct);
 
-router.post("/", authRequired, createProduct);
-router.put("/:id", authRequired, updateProduct);
-router.delete("/:id", authRequired, deleteProduct); */
-
-/* router.get("/category/:categoryName", getProductByCategory)
-router.get("/leaked/:name", getProductByName) */
-
+router.get("/:categoryName", getProductByCategory);
 
 export default router;
