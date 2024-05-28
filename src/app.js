ac6c3eeffdb3a711e84761dd0024651b833093b9
Agustin Morro <agustin.morro@gmail.com>
2024-05-27 21:38:13 -0300

    fix cors

--- src/app.js
+++ src/app.js
@@ -6,10 +6,7 @@ import fileUpload from "express-fileupload";
 import productsRoutes from "./routes/products.routes.js";
 
 const app = express();
-app.use(cors({
-    origin:"http://localhost:5173",
-    credentials: true,
-}))
+app.use(cors())
 
 app.use(morgan("dev"));
 app.use(express.json());
