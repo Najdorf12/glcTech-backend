192a707d86b7eb8ccac60f7f421a5a63fadfc986
Agustin Morro <agustin.morro@gmail.com>
2024-06-03 19:56:22 -0300

    fix getProductsByCategory

--- src/controllers/products.controller.js
+++ src/controllers/products.controller.js
@@ -55,13 +55,16 @@ export const updateProduct = async (req, res) => {
 
 export const getProductByCategory = async (req, res) => {
   const category = req.params.categoryName;
-  const products = await Product.find();
-  const productsFilter = products.filter(
-    (product) => product.category === category
-  );
-  if (!productsFilter)
-    return res.status(404).json({ message: "Product not found" });
-  res.json(productsFilter);
+  try {
+    const products = await Product.find();
+    const productsFilter = products.filter(
+      (product) => product.category === category
+    );
+    if (!productsFilter) return res.status(404).json({ message: "Product not found" });
+    res.json(productsFilter);
+  } catch (error) {
+    console.error(error);
+  }
 };
 
 export const getProduct = async (req, res) => {
