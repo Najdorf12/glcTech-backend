c760491c98074aedad8d0c593a6f303cdcb7e744
Agustin Morro <agustin.morro@gmail.com>
2024-06-25 12:52:00 -0300

    add conection mongodb

--- src/db.js
+++ src/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 export const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO);
+    await mongoose.connect("mongodb+srv://agustinmorro:ZPkTh67XbR9RfNBA@glctechdb.hu0tozu.mongodb.net/?retryWrites=true&w=majority&appName=glctechdb");
     console.log("conexion a mongo exitosa")
   } catch (error) {
     console.log(error);
