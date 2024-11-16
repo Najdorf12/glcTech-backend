6080b63bd7a9d0af33a98a0f2803735f3576b7cb
Najdorf12 <agustin.morro@gmail.com>
2024-11-16 17:23:58 -0300

    add styles

--- src/app.js
+++ src/app.js
@@ -11,7 +11,7 @@ import usdPriceRoutes from "./routes/usdPrice.routes.js";
 
 const app = express();
 app.use(cors({/*  http://localhost:5173  https://www.grupolacomunidad.com.ar*/
-    origin:"https://www.grupolacomunidad.com.ar",
+    origin:"http://localhost:5173",
     credentials: true,
 }));
 
